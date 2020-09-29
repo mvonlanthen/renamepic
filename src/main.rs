3fa5eb2386ec1c57519ead1418f9b2beb3746f2a
Marcel <ubuntu@ip-172-31-33-211.eu-west-1.compute.internal>
2020-09-29 18:02:00 +0000

    on going work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::fs;
-use std::any::type_name;
+// use std::fs;
+// use std::any::type_name;
+use std::path;
 
 extern crate clap;
 use clap::{Arg, App};
@@ -27,12 +28,28 @@ fn main() {
         .get_matches();
 
     // let pictures_dir = matches.value_of("dir").unwrap_or("input.txt");
+    
+
+    // Check if the folder passed as argument exists
     let pictures_dir = matches.value_of("dir").unwrap();
-    println!("The dir passed is: {}", pictures_dir);
+    // println!("The dir passed is: {}", pictures_dir);
+    if path::Path::new(pictures_dir).exists()==true {
+        println!("{} exists", pictures_dir);
+    } else {
+        println!("{} is not a valid directory. exit", pictures_dir);
+        std::process::exit(1)
+    }
+
+    // list all the jpg in picture
+        
+
+
+
 
     let x = 5;
-    let y = 5.4;
+    println!("the value of x is: {}", x)
+    // let y = 5.4;
 
-    println!("{}",type_name::<i32>());
-    println!("{}",type_name::<f32>());
+    // println!("{}",type_name::<i32>());
+    // println!("{}",type_name::<f32>());
 }
