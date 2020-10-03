f774d559c166dc1b402b105a828380114d0c3fca
mvonlanthen <marcel@t440s-marcel.localdomain>
2020-10-03 20:02:00 +0200

    Fixing middle-string flag behaviour

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # renamepic
-Rename a list of pictures according the exif tags available. My very first "real" Rust application. More a testing ground than anything else!
+
+Create copies of pictures in a directory. The copies are names 
+
+    YYYY-MM-DD HH:MM:SS - orignal_name.original_ext
+
+where `YYYY-MM-DD HH:MM:SS` is the DateTimeOriginal tag in the picture exif and `orignal_name.original_ext` the original name of the picture. When scanning the directory, only the following picture type are processed: jpg, JPG, jpeg and JPEG.
+
+## installation
+
+### from the release
+
+### from the source
+1. Download and install the rust toolchain available [here](https://www.rust-lang.org/tools/install).
+2. Clone this repository.
+3. Go to the cloned repo and compile `renamepics` with  
+
+
+## Notes
+* This is my very first "real" Rust application. More a testing ground than anything else! Feel free to comment  (good or bad) the code.
--- src/main.rs
+++ src/main.rs
@@ -22,24 +22,30 @@ fn main() {
                  .takes_value(true)
                  .required(true)
                  .help("Folder with pictures to rename"))
+        .arg(Arg::with_name("middle_string")
+                 .short("m")
+                 .long("middle-string")
+                 .takes_value(true)
+                 .required(false)
+                 .help("add a middle string between the exif datetime and the picture filename"))
         .arg(Arg::with_name("list_exif")
-                 .short("le")
+                 .short("l")
                  .long("list-exif")
                  .takes_value(true)
                  .required(false)
-                 .help("list available exifs for a given picture pass after `le` flag"))
+                 .help("list available exifs for a given picture pass after `le` \
+                        flag (not implemented yet)"))
         .arg(Arg::with_name("target_dir")
-                 .short("td")
+                 .short("t")
                  .long("taget-dir")
                  .takes_value(false)
                  .required(false)
-                 .default_value("false")
-                 .help("sepcify the traget directory"))
+                 .help("sepcify the traget directory (not implemented yet)"))
         .get_matches();
 
     
     // Check if the folder passed as argument exists
-    let foo = matches.value_of("target_dir").unwrap();
+    // let foo = matches.value_of("target_dir").unwrap();
     let pictures_dir = path::Path::new(matches.value_of("dir").unwrap());
     if pictures_dir.exists()==false {
         println!("`{}` is not a valid directory. exit", pictures_dir.display());
@@ -59,31 +65,29 @@ fn main() {
         };
 
         if is_jpg {
+            // get DateTimeOriginal exif tag from the jpg 
             let file = fs::File::open(&path).unwrap();
             let mut bufreader = std::io::BufReader::new(&file);
             let exifreader = exif::Reader::new();
             let exif = exifreader.read_from_container(&mut bufreader).unwrap();
-            // for f in exif.fields() {
-            //     println!("{} {} {}",
-            //                 f.tag, f.ifd_num, f.display_value().with_unit(&exif));
-            // }
             let datetime = exif.get_field(
                 exif::Tag::DateTimeOriginal, 
                 exif::In::PRIMARY
             ).unwrap();
             let dt_string = datetime.display_value().to_string();
 
-            // println!("filename: {}, datetime: {}", 
-            //     &path.display(), 
-            //     dt_string
-            // );
-
-            // copy the jpg with it's new filename.
+            // Prepare the target name path for the jpeg
             let base_path = path.parent().unwrap().to_str().unwrap();
             let filename = path.file_name().and_then(ffi::OsStr::to_str).unwrap();
-            // let mut tgt_path = String::new();
-            let tgt_path = format!("{}/{} - {}", base_path, dt_string, filename);
-            // let res = fs::copy(path, tgt_path);
+            let middle_string = match matches.value_of("middle_string") {
+                Some(s) => format!(" - {}", s),
+                None => String::from(""),
+            };
+            let tgt_path = format!(
+                "{}/{}{} - {}", base_path, dt_string, middle_string, filename
+            );
+
+            // copy the picture to its new location
             let is_successful = match fs::copy(&path, &tgt_path) {
                 Ok(_nb_bytes) => true,
                 Err(_error) => false,
